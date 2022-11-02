8426838f1bb5634b9e9957921bfe995a7bd3fe82
TimaKaschapov <114010564+TimaKaschapov@users.noreply.github.com>
2022-11-02 19:45:14 +0500

    Add files via upload

--- src/js/anim.js
+++ src/js/anim.js
@@ -14,21 +14,3 @@
   for (let elm of elements) {
     observer.observe(elm);
   }*/
-  const time = 5000; // ms
-  const step = 1; 
-  
-  function outNum(num, elem) {
-      let l = document.querySelector('#' + elem);
-      n = 0;
-      let t = Math.round(time/(num/step));
-      let interval = setInterval(() => {
-          n = n + step;
-          if (n == num) {
-              clearInterval(interval);
-          }
-          l.innerHTML = n;
-      },
-          t);
-  }
-  
-  outNum(150, 'by');
