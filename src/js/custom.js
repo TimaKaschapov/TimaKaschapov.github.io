602a2c70075bb293d34b7e9fab8ff623412ba3e5
TimaKaschapov <114010564+TimaKaschapov@users.noreply.github.com>
2022-11-05 00:44:52 +0500

    Add files via upload

--- src/js/custom.js
+++ src/js/custom.js
@@ -37,41 +37,33 @@ $(document).ready(function () {
         $('.image-link').magnificPopup({type:'image'});
       });
 
-      window.addEventListener('load', function() { //дожидаемся полной загрузки страницы
-        if (getCookie('modal_shown') == null) { //проверяем наличие куки и если её нет, то
-            setTimeout(function() { //ждём 15 секунд
-                setCookie('modal_shown', 'yes', 1); //ставим куку
-                $('#modal-order').modal('show'); //вызываем модально окно
-            }, 15000);
+      $(document).ready(function(){
+        var pp     = $("#pp"),
+            bg     = $("#pp-bg");
+     
+        function pp_hide(pp, bg){
+            pp.animate({top: "-550px"}, 1000, function(){bg.fadeOut(1000);});
+        }
+     
+        function pp_show(pp, bg){
+            pp.animate({top: "150px"}, 2000);
+            bg.fadeIn(1000).click(function(){pp_hide(pp, bg)});
+        }
+     
+        if ( $.cookie('visit') == undefined ){
+          setTimeout(function(){
+            $.cookie('visit', true);
+            pp_show(pp, bg);
+        }, 1000);
+        } else {
+            $('body').append('<h1>Вы у нас уже были :)</h1><a href="#" id="remove_cookie">Удалить куки!</a>');
+            $('#remove_cookie').click(function(e){
+                e.preventDefault();
+                $.removeCookie('visit');
+                $(this).after("<span>Куки удалены успешно! Перезагрузите страницу :)</span>").remove();
+            });
         }
     });
-    
-    function setCookie(name, value, days) {
-        var expires = '';
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = '; expires=' + date.toUTCString();
-        }
-        document.cookie = name + '=' + (value || '') + expires + '; path=/';
-    }
-    
-    function getCookie(name) {
-        var nameEQ = name + '=';
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
-    
-    function eraseCookie(name) {
-        setCookie(name, '', {
-            'max-age': -1
-        });
-    }
 });
 
 
